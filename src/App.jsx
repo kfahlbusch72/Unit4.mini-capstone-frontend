40c079f99f87a464c1a00e437c6984d2e14233a8
karl <karl.fahlbusch7241@gmail.com>
2025-08-01 12:44:51 -0500

    fix the merged app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-import { Routes, Route } from "react-router-dom";
-import FacultyPage from "./pages/FacultyPage";
-import FacultyDetailPage from "./pages/FacultyDetailPage";
-import LoginPage from "./pages/LoginPage";
-import FacultyFormPage from "./pages/FacultyFormPage";
-
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
 import DepartmentsPage from "./pages/DepartmentsPage";
 import DepartmentDetailsPage from "./pages/DepartmentDetailsPage";
 import NewDepartmentPage from "./pages/NewDepartmentPage";
 import EditDepartmentPage from "./pages/EditDepartmentPage";
+import FacultyPage from "./pages/FacultyPage";
+import FacultyDetailPage from "./pages/FacultyDetailPage";
+import FacultyFormPage from "./pages/FacultyFormPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <Router>
+    <>
       <nav style={{ padding: "1rem", backgroundColor: "#222", color: "#fff" }}>
-        <Link to="/departments" style={{ marginRight: "1rem", color: "yellow" }}>Departments</Link>
-        <Link to="/departments/new" style={{ marginRight: "1rem", color: "yellow" }}>New Department</Link>
-        <Link to="/departments/1" style={{ marginRight: "1rem", color: "yellow" }}>Details (ID 1)</Link>
-        <Link to="/departments/1/edit" style={{ color: "yellow" }}>Edit (ID 1)</Link>
+        <Link
+          to="/departments"
+          style={{ marginRight: "1rem", color: "yellow" }}
+        >
+          Departments
+        </Link>
+        <Link
+          to="/departments/new"
+          style={{ marginRight: "1rem", color: "yellow" }}
+        >
+          New Department
+        </Link>
+        <Link to="/faculty" style={{ marginRight: "1rem", color: "yellow" }}>
+          Faculty
+        </Link>
+        <Link
+          to="/faculty/new"
+          style={{ marginRight: "1rem", color: "yellow" }}
+        >
+          Add Faculty
+        </Link>
+        <Link to="/login" style={{ color: "yellow" }}>
+          Admin Login
+        </Link>
       </nav>
+
+      <h1 style={{ textAlign: "center", marginTop: "1rem" }}>Welcome to FSU</h1>
 
       <Routes>
         <Route path="/" element={<Navigate to="/departments" />} />
@@ -26,15 +46,10 @@ function App() {
         <Route path="/departments/new" element={<NewDepartmentPage />} />
         <Route path="/departments/:id" element={<DepartmentDetailsPage />} />
         <Route path="/departments/:id/edit" element={<EditDepartmentPage />} />
-      </Routes>
-    </Router>
-    <>
-      <h1>Welcome to FSU</h1>
-      <Routes>
         <Route path="/faculty" element={<FacultyPage />} />
         <Route path="/faculty/:id" element={<FacultyDetailPage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/faculty/new" element={<FacultyFormPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
